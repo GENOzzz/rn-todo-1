b18832877d3fd04060eddd160c31d27c1fb7bac6
GENO_P <dsa01282@naver.com>
2023-01-05 09:53:22 +0900

    input 컴포넌트 수정
    포커스 여부에 따라 스타일 변경

---
+++ src/components/Input2.js
@@ -0,0 +1,72 @@
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import PropTypes from 'prop-types';
+import { GRAY, PRIMARY } from '../colors';
+import { useState } from 'react';
+
+export const KeyboardTypes = {
+  DEFAULT: 'default',
+  EMAIL: 'email-address',
+};
+
+export const ReturnKeyTypes = {
+  DONE: 'done',
+  Next: 'next',
+};
+
+const Input = ({ title, placeholder, ...props }) => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <Text style={[styles.title, isFocused && styles.focusedTitle]}>
+        {title}
+      </Text>
+      <TextInput
+        {...props}
+        style={[styles.input, isFocused && styles.focusedInput]}
+        placeholder={placeholder ?? title}
+        placeholderTextColor={GRAY.DEFAULT}
+        autoCapitalize="none"
+        autoCorrect={false}
+        textContentType="none"
+        keyboardAppearance="light"
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+      />
+    </View>
+  );
+};
+
+Input.PropTypes = {
+  title: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    paddingHorizontal: 20,
+    marginVertical: 10,
+  },
+  title: {
+    marginBottom: 4,
+  },
+  focusedTitle: {
+    fontWeight: '600',
+    color: PRIMARY.DEFAULT,
+  },
+  input: {
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    height: 42,
+    borderColor: GRAY.DEFAULT,
+  },
+  focusedInput: {
+    borderWidth: 2,
+    borderColor: PRIMARY.DEFAULT,
+    color: PRIMARY.DEFAULT,
+  },
+});
+
+export default Input;
--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,8 +1,12 @@
 import { Image, StyleSheet, Text, View } from 'react-native';
-import Input, { KeyboardTypes, ReturnKeyTypes } from '../components/Input';
+import Input, { KeyboardTypes, ReturnKeyTypes } from '../components/Input2';
+import { useState } from 'react';
 import SafeInputView from '../components/SafeInputView';
 
 const SignInScreen = () => {
+  let [email, setEmail] = useState('');
+  let [password, setPassword] = useState('');
+
   return (
     <SafeInputView>
       <View style={styles.container}>
@@ -13,8 +17,15 @@ const SignInScreen = () => {
           placeholder="mail@mail.com"
           keyboardType={KeyboardTypes.EMAIL}
           returnKeyType={ReturnKeyTypes.Next}
+          value={email}
+          onChangeText={(email) => setEmail(email.trim())}
         />
-        <Input title={'비밀번호'} secureTextEntry />
+        <Input
+          title={'비밀번호'}
+          secureTextEntry
+          value={password}
+          onChangeText={(password) => setPassword(password.trim())}
+        />
       </View>
     </SafeInputView>
   );
